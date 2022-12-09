7a8b1c3bef62dfbecb3b345a6d45e5576d133023
Ibrahim M <Syed.IbrahimM@nielseniq.com>
2022-12-09 22:21:35 +0530

    Prescription I

--- src/sections/@dashboard/e-commerce/checkout/PrescriptionList.js
+++ src/sections/@dashboard/e-commerce/checkout/PrescriptionList.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import * as Yup from 'yup';
 import { useCallback, useEffect, useMemo, useState, SyntheticEvent } from 'react';
+import moment from 'moment'
 //
 import { useForm, Controller, useFieldArray  } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
@@ -23,7 +24,15 @@ import {
   TextField
 } from '@mui/material';
 //
-import { LoadingButton, DatePicker } from '@mui/lab';
+import { LoadingButton, DatePicker, DesktopDatePicker  } from '@mui/lab';
+import MenuItem from '@mui/material/MenuItem';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+
 // utils
 import getColorName from '../../../../utils/getColorName';
 import { fCurrency } from '../../../../utils/formatNumber';
@@ -60,86 +69,75 @@ export default function PrescriptionList({ drugs, onDelete, onIncreaseQuantity, 
   const [currentPrescription, setCurrentPrescription] = useState([]);
   const { user: _userList } = useUsers();
   const [user, setUser] = useState([])
+  const [defaultUser, setDefaultUser] = useState(_userList.filter(user => user.isStaff)[0].id)
+
+  const duration = [
+    {id: 1, label: 'day(s)'},
+    {id: 2, label: 'week(s)'},
+    {id: 3, label: 'month(s)'},
+    {id: 4, label: 'year(s)'}
+  ]
+  const validationSchema = Yup.object().shape({
+    cart: Yup.array().of(
+      Yup.object().shape({
+        when: Yup.string().required('This feild is required'),
+        durationUnit: Yup.string().required('missing feild is not found'),
+        duration: Yup.string().required('missing feild is not found'),
+        morning: Yup.string().required('missing feild is not found'),
+        noon: Yup.string().required('missing feild is not found'),
+        night: Yup.string().required('missing feild is not found'),
+      })
+    ),
+  });
+
+  const defaultValues = {
+    cart: [{ when: "" , durationUnit: duration[0].id, duration: "0", morning: "0", noon: "0", night: "0" }],
+    orderedBy: defaultUser
+  } 
+  
+  const {
+    register,
+    handleSubmit,
+    formState,
+    watch,
+    reset,
+    control
+  } = useForm({
+    resolver: yupResolver(validationSchema),
+    defaultValues,
+  });
+  const { errors } = formState;
+  const { fields, append, prepend, remove, replace } = useFieldArray({
+    name: "cart",
+    control,
+    rules: {
+      required: "Please append at least 1 item"
+    }
+  });
+
+  useEffect(() => {
+    console.log(errors)
+  },[errors])
 
   useEffect(() => {
     const staff =  _userList.filter(user => user.isStaff)
     setUser(staff)
   },[_userList])
 
-  /* const NewPrescriptionSchema = Yup.object().shape({
-    strength: Yup.string().required('Strength is required'),
-    duration: Yup.string().required('Duration is required'),
-    morning: Yup.string().required('Morning is required'),
-    noon: Yup.string().required('Noon is required'),
-    night: Yup.string().required('Night is required'),
-    instruction: Yup.string(),
-    orderedBy: Yup.string(),
-    orderedOn: Yup.string(),
-  }); */
-
-  const newSchema = Yup.array().of(
-    Yup.object().shape({
-      strength: Yup.string().required('Strength is required'),
-      duration: Yup.string().required('Duration is required'),
-      morning: Yup.string().required('Morning is required'),
-      noon: Yup.string().required('Noon is required'),
-      night: Yup.string().required('Night is required'),
-      instruction: Yup.string(),
-      orderedBy: Yup.string(),
-      orderedOn: Yup.string(),
-    })
-  )
-
-  const formOptions = useForm({
-    resolver: yupResolver(newSchema),
-    // defaultValues,
-  });
-
-  // functions to build form returned by useForm() and useFieldArray() hooks
-  const { register, control, handleSubmit, reset, formState, watch } = useForm(formOptions);
-  const { errors } = formState;
-  const { fields, append, remove } = useFieldArray({ name: 'prescription', control });
-  /* const defaultValues = useMemo(
-    () => ({
-      strength: currentPrescription?.strength || '',
-      duration: currentPrescription?.duration || '',
-      morning: currentPrescription?.morning || 0,
-      noon: currentPrescription?.noon || 0,
-      night: currentPrescription?.night || 0,
-      instruction: currentPrescription?.instruction || '',
-      orderedBy: currentPrescription?.duration || '',
-      orderedOn: currentPrescription?.orderedOn || new Date(),
-    }),
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [currentPrescription]
-  ); */
-
   useEffect(() => {
-
+    drugs?.forEach(drug => {
+      replace([{itemName: 'test'},{itemName: 'test2'}]) 
+    })
     setCurrentPrescription(drugs)
   },[drugs])
 
-  /* const methods = useForm({
-    resolver: yupResolver(NewPrescriptionSchema),
-    // defaultValues,
-  });
+  const [dateValue, setDateValue] = useState(moment(new Date()).format());
 
-  const {
-    reset,
-    watch,
-    control,
-    setValue,
-    getValues,
-    handleSubmit,
-    formState: { isSubmitting },
-  } = methods; */
-
-  const onSubmit = async (data) => {
-    try {
-      console.log(data);
-    } catch (error) {
-      console.error(error);
-    }
+  const handleChange = (newValue) => {
+    setDateValue(moment(newValue).format());
+  };
+  const handleStaffChange = (newValue) => {
+    setDefaultUser(newValue.target.value);
   };
 
   const handleInstruction = (i) => {
@@ -149,32 +147,118 @@ export default function PrescriptionList({ drugs, onDelete, onIncreaseQuantity, 
     console.log(currentPrescription)
   }
 
+  
+function onSubmit(data) {
+    // display form data on success
+    console.log(data);
+}
+
   return (
     <>
-    <FormProvider methods={formOptions} onSubmit={handleSubmit(onSubmit)}>
-      {currentPrescription.map((drug, i) => {
-        const { id, itemName, strength, duration, morning, noon, night, intake, instruction, showInstruction } = drug;
-        return (
-          <Card sx={{ p: 3, display: 'grid', m: 1, }}>
-            <Stack
+    <form
+        onSubmit={handleSubmit((data) => {
+          console.log("Submit data", data);
+        })}
+      >
+        {currentPrescription.map((field, index) => {
+          return (
+            <section key={field.id}>
+              
+              <Stack
+                sx={{
+                  display: 'grid',
+                  columnGap: 2,
+                  rowGap: 3,
+                  padding: 2,
+                  justifyItems: 'center',
+                  alignItems: 'center',
+                  gridTemplateColumns: { xs: 'repeat(2, 1fr)', sm: 'repeat(2, 1fr)' },
+                }}
+              >
+                <Typography noWrap variant="subtitle2" sx={{ maxWidth: 240 }}>
+                  {/* {cart[${i}]itemName} {`- ${prescription[${i}]strength}`} */}
+                  {field.itemName} {field.strength ? '-' : ''}` {field.strength}
+                </Typography>
+                <RadioGroup
+                  sx={{
+                    display: 'grid',
+                    columnGap: 2,
+                    rowGap: 1,
+                    gridTemplateColumns: { xs: 'repeat(2, 1fr)', sm: 'repeat(2, 1fr)' },
+                  }}
+                  aria-labelledby="demo-radio-buttons-group-label"
+                  name="radio-buttons-group"
+                  {...register(`cart.${index}.when`, { required: true })}
+                >
+                  <FormControlLabel value="before" control={<Radio />} label="Before Food" />
+                  <FormControlLabel value="after" control={<Radio />} label="After Food" />
+                </RadioGroup>
+              </Stack>
+              <Stack
               sx={{
-                display: 'grid',
+                display: 'flex',
                 columnGap: 2,
+                flexDirection: 'row',
                 rowGap: 3,
-                gridTemplateColumns: { xs: 'repeat(2, 1fr)', sm: 'repeat(2, 1fr)' },
+                padding: 2,
+                gridTemplateColumns: { xs: 'repeat(5, 1fr)', sm: 'repeat(5, 1fr)' },
               }}
             >
-            <Typography noWrap variant="subtitle2" sx={{ maxWidth: 240 }}>
-                {itemName} {`- ${strength}`}
-              </Typography>
-              <RHFRadioGroup
-                  name="intake"
-                  options={INTAKE_OPTION}
-                  sx={{
-                    '& .MuiFormControlLabel-root': { mr: 4 },
-                  }}
-                />
+              <TextField id="outlined-basic" label="Duration" {...register(`cart.${index}.duration`, { required: true })} variant="outlined" />
+              <Controller
+                name={`cart.${index}.durationUnit`}
+                control={control}
+                render={({ field }) => (
+                  <Select
+                  {...field}
+                  {...register(`cart.${index}.durationUnit`, { required: true })} 
+                >
+                  {duration.map((option) => (
+                    <MenuItem key={option.id} value={option.id}>{option.label}</MenuItem>
+                  ))}
+                </Select>
+                )}
+              />
+              <TextField id="outlined-basic" label="Morning" {...register(`cart.${index}.morning`, { required: true })} variant="outlined" />
+              <TextField id="outlined-basic" label="Noon" {...register(`cart.${index}.noon`, { required: true })} variant="outlined" />
+              <TextField id="outlined-basic" label="Night" {...register(`cart.${index}.night`, { required: true })} variant="outlined" />
             </Stack>
+            </section>
+          );
+        })}
+        <Stack sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-evenly', p: 2, position: 'fixed', bottom: 0, left: '25%', right: '10%', background: '#FFF', width: '50%', gap: '20px' }}>
+          <Controller
+            name="orderedBy"
+            control={control}
+            render={({ field }) => (
+              <Select
+              {...field}
+              {...register(`orderedBy`, { required: true })}
+            >
+              {user.map((option) => (
+                <MenuItem key={option.id} value={option.id}>{option.firstName} {option.lastName}</MenuItem>
+              ))}
+            </Select>
+            )}
+          />
+          <DesktopDatePicker
+            {...register(`orderedOn`, { required: true })}
+            label="Date desktop"
+            value={dateValue}
+            inputFormat="dd/MM/yyyy"
+            onChange={handleChange}
+            renderInput={(params) => <TextField {...params} />}
+          />
+          
+          <LoadingButton type="submit" variant="contained" size="large" >
+            Save
+          </LoadingButton>
+        </Stack>
+        
+      </form>
+    {/* <FormProvider methods={formOptions} onSubmit={handleSubmit(onSubmit)}>
+      {fields.map((item, i) => (
+          <Card key={item.id} sx={{ p: 3, display: 'grid', m: 1, }}>
             <Stack
               sx={{
                 display: 'flex',
@@ -184,76 +268,20 @@ export default function PrescriptionList({ drugs, onDelete, onIncreaseQuantity, 
                 gridTemplateColumns: { xs: 'repeat(5, 1fr)', sm: 'repeat(5, 1fr)' },
               }}
             >
-              <RHFTextField name={`prescription[${i}]duration`} {...register(`prescription.${i}.duration`)} label="Duration" />
-              <RHFTextField name={`prescription[${i}]morning`} {...register(`prescription.${i}.morning`)} label="Morning" />
-              <RHFTextField name={`prescription[${i}]noon`} {...register(`prescription.${i}.noon`)} label="Noon" />
-              <RHFTextField name={`prescription[${i}]night`} {...register(`prescription.${i}.night`)} label="Night" />
+              <RHFTextField name={`prescription.${i}.duration`} {...register(`prescription.${i}.duration`)} ref={null} label="Duration" />
+              <RHFTextField name={`prescription.${i}.morning`} {...register(`prescription.${i}.morning`)} ref={null} label="Morning" />
+              <RHFTextField name={`prescription.${i}.noon`} {...register(`prescription.${i}.noon`)} ref={null} label="Noon" />
+              <RHFTextField name={`prescription.${i}.night`} {...register(`prescription.${i}.night`)} ref={null} label="Night" />
               <div>
-              <IconButton>
-                <Iconify icon={'eva:trash-2-outline'} />
-              </IconButton>
-              </div>
-            </Stack>
-            <Stack
-              sx={{
-                display: 'grid',
-                columnGap: 2,
-                rowGap: 3,
-                gridTemplateColumns: { xs: 'repeat(2, 1fr)', sm: 'repeat(2, 1fr)' },
-              }}
-            >
-            <Link
-              component="button"
-              variant="body2"
-              onClick={() => handleInstruction(i)}
-            >
-              + add instruction
-            </Link>
-            </Stack>
-            <Stack
-              sx={{
-                display: 'grid',
-                columnGap: 2,
-                rowGap: 3,
-                gridTemplateColumns: { xs: 'repeat(2, 1fr)', sm: 'repeat(2, 1fr)' },
-              }}
-            >
-            {( showInstruction && 
-              <RHFTextField name={`instruction-${i}`} label="Instruction" multiline rows={3} />
-            )}
-            </Stack>
-            <Stack sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-evenly', p: 2, position: 'fixed', bottom: 0, left: '10%', right: '10%', background: '#FFF', width: '80%', gap: '20px' }}>
-              <RHFSelect name={`orderedBy-${i}`} label="Ordered By" placeholder="Ordered By">
-                {user.map((option) => (
-                  <option key={option.id} value={option.id}>
-                    {option.firstName} {option.lastName}
-                  </option>
-                ))}
-              </RHFSelect>
-              <Controller
-                name={`orderedOn-${i}`}
-                control={control}
-                render={({ field, fieldState: { error } }) => (
-                  <DatePicker
-                    label="Ordered On"
-                    onChange={(newValue) => {
-                      field.onChange(newValue);
-                    }}
-                    renderInput={(params) => (
-                      <TextField {...params} fullWidth error={!!error} helperText={error?.message} />
-                    )}
-                  />
-                )}
-              />
               <LoadingButton type="submit" variant="contained" size="large" >
                 Save
               </LoadingButton>
+              </div>
             </Stack>
           </Card>
-          
-        );
-      })}
-    </FormProvider>
+      ))}
+    </FormProvider> */}
+    
     <Stack
       sx={{
         display: 'grid',
